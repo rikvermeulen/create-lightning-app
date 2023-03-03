bc41389089946e096accd6db858337df3f9e48e9
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-03-03 11:36:00 +0100

    ci: add index.ts cli with logging

---
+++ cli/src/index.ts
@@ -0,0 +1,14 @@
+import { logger } from './utils/logger.js';
+
+const main = async () => {};
+
+main().catch((err) => {
+  logger.error('Aborting installation...');
+  if (err instanceof Error) {
+    logger.error(err);
+  } else {
+    logger.error('An unknown error has occurred. Please open an issue on github with the below:');
+    console.log(err);
+  }
+  process.exit(1);
+});

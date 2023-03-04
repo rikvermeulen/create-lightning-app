92cf97ace1fbf22813b96bad61f390d9594c6280
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-03-04 13:37:20 +0100

    feat: way to add deps to lightning in install

--- cli/src/dependencies/index.ts
+++ cli/src/dependencies/index.ts
@@ -1,23 +1,41 @@
+import { type PackageManager } from '../utils/getUserPackageManager.js';
+import { prismaInstaller } from './installers/primsa.js';
+
 export const availablePackages = ['nextAuth', 'prisma', 'tailwind'] as const;
 export type AvailablePackages = (typeof availablePackages)[number];
+
+export type Installer = (opts: InstallerOptions) => void;
 
 export type installedPackages = {
   [pkg in AvailablePackages]: {
     inUse: boolean;
+    installer: Installer;
   };
 };
+
+export const tailwindInstaller: Installer = ({ projectDir }) => {
+  console.log(projectDir);
+};
+
+export interface InstallerOptions {
+  projectDir: string;
+  packageManager: PackageManager;
+  noInstall: boolean;
+  packages?: installedPackages;
+  projectName?: string;
+}
 
 export const dependencies = (packages: AvailablePackages[]) => ({
   nextAuth: {
     inUse: packages.includes('nextAuth'),
-    // installer: nextAuthInstaller,
+    installer: tailwindInstaller,
   },
   prisma: {
     inUse: packages.includes('prisma'),
-    // installer: prismaInstaller,
+    installer: prismaInstaller,
   },
   tailwind: {
     inUse: packages.includes('tailwind'),
-    // installer: tailwindInstaller,
+    installer: tailwindInstaller,
   },
 });
---
+++ cli/src/utils/addDependency.ts
@@ -0,0 +1,33 @@
+import path from 'path';
+import fs from 'fs-extra';
+import { type PackageJson } from 'type-fest';
+import {
+  dependencyVersions,
+  type AvailableDependencies,
+} from '../dependencies/dependencyVersions.js';
+import sortPackageJson from 'sort-package-json';
+
+export const addPackageDependency = (opts: {
+  dependencies: AvailableDependencies[];
+  devMode: boolean;
+  projectDir: string;
+}) => {
+  const { dependencies, devMode, projectDir } = opts;
+
+  const pkgJson = fs.readJSONSync(path.join(projectDir, 'package.json')) as PackageJson;
+
+  dependencies.forEach((pkgName) => {
+    const version = dependencyVersions[pkgName];
+
+    if (devMode && pkgJson.devDependencies) {
+      pkgJson.devDependencies[pkgName] = version;
+    } else if (pkgJson.dependencies) {
+      pkgJson.dependencies[pkgName] = version;
+    }
+  });
+  const sortedPkgJson = sortPackageJson(pkgJson);
+
+  fs.writeJSONSync(path.join(projectDir, 'package.json'), sortedPkgJson, {
+    spaces: 2,
+  });
+};

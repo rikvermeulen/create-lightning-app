b73ece5cfa153d7457e980110852e7eb899e4e47
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-03-08 10:41:11 +0100

    fix: deleted old files

--- cli/src/dependencies/installers/index.ts
+++
@@ -1,5 +0,0 @@
-import { prismaInstaller } from './prisma.js';
-import { pwaInstaller } from './pwa.js';
-import { vitestInstaller } from './vitest.js';
-
-export { prismaInstaller, pwaInstaller, vitestInstaller };
